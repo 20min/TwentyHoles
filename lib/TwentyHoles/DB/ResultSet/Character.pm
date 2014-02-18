94e57e521fca3dfa0068a0e66d62ab46c2374287
Carl Johnstone <fade@fadetoblack.me.uk>
2014-02-18 22:37:37 +0000

    create related systems in a better way

--- lib/TwentyHoles/DB/ResultSet/Character.pm
+++ lib/TwentyHoles/DB/ResultSet/Character.pm
@@ -10,15 +10,18 @@ sub seen_character {
     my $self = shift;
     my $opts = shift;
 
+    my $now = DateTime->now(); #UTC by default
+
     my $character = $self->find_or_new({
-        'character_id'      => $opts->{'character_id'},
+        'character_id' => $opts->{'character_id'},
     });
     $character->character_name( $opts->{'character_name'} );
     $character->ship_name( $opts->{'ship_name'} );
-    $character->last_seen( DateTime->now() ); #UTC default
-    $character->find_or_create_related('system_name' => {
+    $character->last_seen( $now );
+    my $system = $self->related_resultset('system_name')->find_or_new({
         'name' => $opts->{'system_name'},
     });
+    $character->system_name( $system );
     $character->update_or_insert;
 }
 1;
